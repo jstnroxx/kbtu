34b362c668f9cc9a8fc5fa610036045eae04d045
jstnroxx <ms325358@gmail.com>
2026-01-31 22:42:59 +0500

    add tuple code examples
    pp2

---
+++ programming-principles-2/practice02/tuples/python-tuples.py
@@ -0,0 +1,33 @@
+# A tuple is a collection which is ordered and unchangeable.
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+# Tuples allow duplicate values.
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+# Print the number of items in the tuple.
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+# One item tuple, remember the comma.
+thistuple = ("apple",)
+print(type(thistuple))
+
+# NOT a tuple.
+thistuple = ("apple")
+print(type(thistuple))
+
+# String, int and boolean data types.
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+# A tuple with strings, integers and boolean values.
+tuple1 = ("abc", 34, True, 40, "male")
+
+# Using the tuple() method to make a tuple.
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
+
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
